2739828a534758e69b43f204adbecbf4f3ba07f7
Graham McVea <graham.mcvea@gmail.com>
2014-10-07 11:50:35 +0100

    Setting up WebAPI, Ninject, Elmah and help page. Updatingnuget packages for all solution.

--- WonderApp.API/Controllers/WonderController.cs
+++ WonderApp.API/Controllers/WonderController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
 using WonderApp.Models;
+using Elmah.Contrib.WebApi;
 
 namespace WonderApp.Controllers
 {
--- WonderApp.API/Global.asax.cs
+++ WonderApp.API/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Elmah.Contrib.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,7 @@ namespace WonderApp
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Filters.Add(new ElmahHandleErrorApiAttribute());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
