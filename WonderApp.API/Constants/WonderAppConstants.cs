e7f921b205e65c5c92dd61c5b935cc054b82328f
Graham McVea <graham.mcvea@gmail.com>
2014-10-09 16:38:02 +0100

    Web API /api/account/facebook/{accessToken}

---
+++ WonderApp.API/Constants/WonderAppConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WonderApp.Constants
+{
+    public static class WonderAppConstants
+    {
+        public const string Facebook = "facebook";
+        public const string UserAlreadyRegisted = "User is registered with different facebook credentials";
+        public const string CreateUserError = "Error creating user";
+        public const string CreateFacebookDetailsError = "Error adding Facebook details";
+    }
+}
---
+++ WonderApp.API/Models/FacebookUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WonderApp.Models
+{
+    public class FacebookUserModel
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}
