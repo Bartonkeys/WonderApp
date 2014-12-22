50fd0e9516ad6cd96e2b2db41410414e673de4e3
Michael Barr <michaelcbarr@hotmail.com>
2014-12-22 18:07:13 +0000

    kuh

---
+++ WonderApp.API/Models/AuthModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WonderApp.Models
+{
+    public class AuthModel
+    {
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+
+    }
+}
