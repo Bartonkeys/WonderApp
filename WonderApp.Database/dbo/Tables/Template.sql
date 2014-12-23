aaabb088966a045969ff33249c842a1cf44ffddd
Michael Barr <michaelcbarr@hotmail.com>
2014-12-23 11:59:56 +0000

    - more minor tidyups

--- WonderApp.Database/dbo/Tables/Template.sql
+++ WonderApp.Database/dbo/Tables/Template.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Template] (
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [Name]      NCHAR (200)    NULL,
+    [Name]      NVARCHAR (200) NULL,
     [Thumbnail] NVARCHAR (200) NULL,
     [File]      NVARCHAR (200) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
