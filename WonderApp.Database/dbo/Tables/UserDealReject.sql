948f05c851dc34d53937d83d724ab01216bc7c54
Michael Barr <michaelcbarr@hotmail.com>
2015-01-06 22:24:54 +0000

    - Emails sent dependent on user prefs

--- WonderApp.Database/dbo/Tables/UserDealReject.sql
+++ WonderApp.Database/dbo/Tables/UserDealReject.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[UserDealReject] (
     [MyRejectUsers_Id] NVARCHAR (128) NOT NULL,
     [MyRejects_Id]     INT            NOT NULL,
+    [Timestamp]        DATETIME       DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_UserDealReject] PRIMARY KEY CLUSTERED ([MyRejectUsers_Id] ASC, [MyRejects_Id] ASC),
     CONSTRAINT [FK_UserDealReject_AspNetUser] FOREIGN KEY ([MyRejectUsers_Id]) REFERENCES [dbo].[AspNetUsers] ([Id]),
     CONSTRAINT [FK_UserDealReject_Deal] FOREIGN KEY ([MyRejects_Id]) REFERENCES [dbo].[Deals] ([Id])
 );
+
+
 
 
 GO
--- WonderApp.Database/dbo/Tables/UserDealWonders.sql
+++ WonderApp.Database/dbo/Tables/UserDealWonders.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[UserDealWonders] (
     [MyWonderUsers_Id] NVARCHAR (128) NOT NULL,
     [MyWonders_Id]     INT            NOT NULL,
+    [Timestamp]        DATETIME       DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_UserDealWonders] PRIMARY KEY CLUSTERED ([MyWonderUsers_Id] ASC, [MyWonders_Id] ASC),
     CONSTRAINT [FK_UserDealWonders_AspNetUser] FOREIGN KEY ([MyWonderUsers_Id]) REFERENCES [dbo].[AspNetUsers] ([Id]),
     CONSTRAINT [FK_UserDealWonders_Deal] FOREIGN KEY ([MyWonders_Id]) REFERENCES [dbo].[Deals] ([Id])
 );
+
+
 
 
 GO
