344a49b04a609da465d2a8c60ff626ea908c1b0a
Michael Barr <michaelcbarr@hotmail.com>
2015-01-23 18:44:59 +0000

    - fixed weird error on title if block in EmailTemplates/MyWondersEmailTemplate.cshtml

--- WonderApp.API/Controllers/EmailController.cs
+++ WonderApp.API/Controllers/EmailController.cs
@@ -16,49 +16,53 @@ namespace WonderApp.Controllers
     [RoutePrefix("api/email")]
     public class EmailController : BaseApiController
     {
+        //NB THIS API CALL HAS BEEN MOVED TO THE WEB PROJECT
+
+
+
         /// <summary>
         /// HTTP POST to send emails
         /// Returns HTTP StatusCode 200.
         /// If error, return Http Status Code 500 with error message.
         /// </summary>
         /// <returns></returns>   
-        [Route("MyWonders")]
-        public async Task<HttpResponseMessage> PostEmailMyWonders([FromBody]AuthModel authModel)
-        {
-            try
-            {
-                if (authModel.Password.Equals("SendMyWonderEmails"))
-                {
+    //    [Route("MyWonders")]
+    //    public async Task<HttpResponseMessage> PostEmailMyWonders([FromBody]AuthModel authModel)
+    //    {
+    //        try
+    //        {
+    //            if (authModel.Password.Equals("SendMyWonderEmails"))
+    //            {
 
-                    //Check time of last send
-                    var twoDaysAgo = DateTime.Now.Subtract(new TimeSpan(2, 0, 0, 0));
+    //                //Check time of last send
+    //                var twoDaysAgo = DateTime.Now.Subtract(new TimeSpan(2, 0, 0, 0));
 
-                    if (DataContext.NotificationEmails.Any() && DataContext.NotificationEmails.Any(e => e.Sent > twoDaysAgo))
-                    {
-                        return Request.CreateResponse(HttpStatusCode.OK, "No need to run yet");
-                    }
+    //                if (DataContext.NotificationEmails.Any() && DataContext.NotificationEmails.Any(e => e.Sent > twoDaysAgo))
+    //                {
+    //                    return Request.CreateResponse(HttpStatusCode.OK, "No need to run yet");
+    //                }
 
 
-                    //TODO - inject dependent on email provider
-                    var emailService = new EmailService();
+    //                //TODO - inject dependent on email provider
+    //                var emailService = new EmailService();
 
-                    await emailService.SendMyWonderEmails(DataContext);
+    //                await emailService.SendMyWonderEmails(DataContext);
 
-                    return Request.CreateResponse(HttpStatusCode.OK);
-                }
+    //                return Request.CreateResponse(HttpStatusCode.OK);
+    //            }
 
-                else
-                {
-                    return Request.CreateErrorResponse(HttpStatusCode.Forbidden, "You are not authorized to execute this command");
-                }
-            }
+    //            else
+    //            {
+    //                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, "You are not authorized to execute this command");
+    //            }
+    //        }
            
 
-            catch (Exception ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
-            }
+    //        catch (Exception ex)
+    //        {
+    //            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+    //        }
             
-        }
+    //    }
     }
 }
