97cb0da2dead13037ea86006a3fe0d590ad7dece
Graham McVea <graham.mcvea@gmail.com>
2015-03-15 20:54:57 +0000

    Hopefully this time

--- WonderApp.Models/AutoMapperConfiguration/AutoMapperConfigurator.cs
+++ WonderApp.Models/AutoMapperConfiguration/AutoMapperConfigurator.cs
@@ -19,7 +19,7 @@ namespace WonderApp.Models.AutoMapperConfiguration
             Mapper.CreateMap<Deal, DealModel>()
                 .ForMember(m => m.ExpiryDate, 
                     opt => opt.MapFrom(e => e.AlwaysAvailable == true
-                        ? String.Empty : e.ExpiryDate.ToString("dd-MM-YYYY HH:mm:ss")))
+                        ? String.Empty : e.ExpiryDate.ToString("dd-MM-yyyy HH:mm:ss")))
                 .ForMember(e => e.Phone, opt => opt.NullSubstitute(String.Empty))
                 .ForMember(dest => dest.Creator,
                            opts => opts.MapFrom(src => src.AspNetUser));
