1829151c85adcb03e32c9eb925d677cead5f6513
Graham McVea <graham.mcvea@gmail.com>
2015-04-15 16:10:18 +0100

    Turn off optimization as its fucked a load of images

--- WonderApp.Web/App_Start/BundleConfig.cs
+++ WonderApp.Web/App_Start/BundleConfig.cs
@@ -68,7 +68,7 @@ namespace WonderApp.Web
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            //BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
