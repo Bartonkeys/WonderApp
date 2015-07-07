c34a4b82a933aa2e29fe252cbbdce2fbad716844
Graham McVea <graham.mcvea@gmail.com>
2015-07-07 19:49:04 +0100

    RElax password rules

--- WonderApp.API/App_Start/IdentityConfig.cs
+++ WonderApp.API/App_Start/IdentityConfig.cs
@@ -28,10 +28,10 @@ namespace WonderApp
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
                 RequireUppercase = true,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
