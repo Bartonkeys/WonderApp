fa06121b2e55ce85f36519057c92f4f922ab8ad8
Graham McVea <graham.mcvea@gmail.com>
2015-07-08 13:46:41 +0100

    password 8 characters only

--- WonderApp.API/App_Start/IdentityConfig.cs
+++ WonderApp.API/App_Start/IdentityConfig.cs
@@ -32,7 +32,7 @@ namespace WonderApp
                 RequireNonLetterOrDigit = false,
                 RequireDigit = false,
                 RequireLowercase = false,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
